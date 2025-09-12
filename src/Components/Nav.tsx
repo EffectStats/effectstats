7babc08fc647ae66a5a4b253673012996340dc44
Zhi Jun Liang <111754330+Liangzhijun123@users.noreply.github.com>
2025-09-12 10:42:23 -0400

    nav bar completed

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
+
 const Nav = () => {
-    return (
-        <>
-            <div>
-                <p>EffectStats</p>
-            </div>
-        </>
-    );
-}
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="nav-container">
+      {/* Left: Logo */}
+      <div className="nav-left">
+        <p className="logo-text">EffectStats</p>
+      </div>
+
+      {/* Center: Links */}
+      <div className={`nav-menu ${isOpen ? "open" : ""}`}>
+        <div className="nav-links">
+          <a href="#">Home</a>
+          <a href="#">Who we are</a>
+          <a href="#">Services</a>
+          <a href="#">Statistical Tools</a>
+          <a href="#">Contact Us</a>
+        </div>
+      </div>
+
+      {/* Right: Hamburger */}
+      <button onClick={() => setIsOpen(!isOpen)} className="mobile-nav">
+        <img
+          src="/menu.png"
+          style={{ width: "24px", height: "24px" }}
+          alt="menu"
+        />
+      </button>
+    </div>
+  );
+};
 
 export default Nav;
