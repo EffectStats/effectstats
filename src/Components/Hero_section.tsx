5e969397c5971031eb1863a16973cd26407ab082
Liangzhijun123 <Liangzhijun1216@gmail.com>
2025-09-14 14:39:50 -0400

    finished bio-statistical 3 rows part

--- src/Components/Hero_section.tsx
+++ src/Components/Hero_section.tsx
@@ -49,6 +49,96 @@ const HeroSection = () => {
             View Our Services
           </a>
         </div>
+
+        <div className="bio-sections">
+          <div className="bio-container">
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                gap: "24px",
+              }}
+            >
+              <div className="bio-icon">
+                <img src="/stats.png" style={{ width: "50px", height: "50px" }} />
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  gap: "12px",
+                }}
+              >
+                {" "}
+                <p style={{ fontWeight: "600" }}>Bio-statistical Excellence</p>
+                <p className="bio-paragraph">
+                  Strategic level biostatistical support and innovative
+                  statistical science from experienced professionals.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="bio-container">
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                gap: "24px",
+              }}
+            >
+              <div className="bio-icon">
+                <img src="/db.png" style={{ width: "50px", height: "50px" }} />
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  gap: "12px",
+                }}
+              >
+                {" "}
+                <p style={{ fontWeight: "600" }}>Bio-statistical Excellence</p>
+                <p className="bio-paragraph">
+                  Strategic level biostatistical support and innovative
+                  statistical science from experienced professionals.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="bio-container">
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                gap: "24px",
+              }}
+            >
+              <div className="bio-icon">
+                <img src="/bio.png" style={{ width: "50px", height: "50px" }} />
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  gap: "12px",
+                }}
+              >
+                {" "}
+                <p style={{ fontWeight: "600" }}>Bio-statistical Excellence</p>
+                <p className="bio-paragraph">
+                  Strategic level biostatistical support and innovative
+                  statistical science from experienced professionals.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
