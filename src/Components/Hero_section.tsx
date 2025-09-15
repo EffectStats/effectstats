1154a1d72a94e09323d2aa4190c04b70b89235ac
Liangzhijun123 <Liangzhijun1216@gmail.com>
2025-09-15 18:55:13 -0400

    finished story section in home page

--- src/Components/Hero_section.tsx
+++ src/Components/Hero_section.tsx
@@ -190,6 +190,7 @@ const HeroSection = () => {
               flexDirection: "column",
               alignItems: "center",
               gap: "8px",
+              justifyContent: "center",
             }}
           >
             <p
@@ -202,7 +203,7 @@ const HeroSection = () => {
             >
               100%
             </p>
-            <p>Regulatory Compliance</p>
+            <p style={{ whiteSpace: "nowrap" }}>Regulatory Compliance</p>
           </div>
           <div
             style={{
@@ -223,6 +224,156 @@ const HeroSection = () => {
               24/7
             </p>
             <p>Support Available</p>
+          </div>
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "64px",
+            paddingTop: "50px",
+          }}
+        >
+          <p className="story-text">Founded in 2016</p>
+          <div className="story-container">
+            <p className="story-title">Our Story</p>
+            <p className="story-paragraph">
+              Founded in 2016 by experienced biostatisticians, EffectStats
+              provides integrated data science solutions tailored to your
+              clinical development needs.
+            </p>
+          </div>
+
+          <div className="story-div">
+            <div
+              style={{
+                border: "1px solid #006F3E",
+                borderWidth: "1px",
+                borderRadius: "10px",
+                paddingLeft: "30px",
+                paddingRight: "30px",
+                paddingTop: "30px",
+                paddingBottom: "30px",
+              }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: "42px",
+                }}
+              >
+                <div className="bio-icon">
+                  <img
+                    src="/target.png"
+                    style={{ width: "50px", height: "50px" }}
+                  />
+                </div>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "24px",
+                  }}
+                >
+                  <p style={{ fontWeight: "600", fontSize: "24px" }}>
+                    The Challenge We Solve
+                  </p>
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      gap: "16px",
+                    }}
+                  >
+                    <p className="story-p">
+                      With advancements in clinical research and the vibrant
+                      business environment, each biotech/pharmaceutical company
+                      has a unique pathway in clinical development. The main
+                      challenge companies face is balancing varying needs with
+                      investment in internal resources.
+                    </p>
+                    <p className="story-p">
+                      EffectStats was founded to provide the final solution - we
+                      work as your integrated data science team, tailoring our
+                      contribution based on your needs at different stages of
+                      development. With advancements in clinical research and
+                      the vibrant business environment, each
+                      biotech/pharmaceutical company has a unique pathway in
+                      clinical development. The main challenge companies face is
+                      balancing varying needs with investment in internal
+                      resources.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div
+              style={{
+                border: "1px solid #006F3E",
+                borderWidth: "1px",
+                borderRadius: "10px",
+                paddingLeft: "30px",
+                paddingRight: "30px",
+                paddingTop: "30px",
+                paddingBottom: "30px",
+              }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: "42px",
+                }}
+              >
+                <div className="bio-icon">
+                  <img
+                    src="/target.png"
+                    style={{ width: "50px", height: "50px" }}
+                  />
+                </div>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "24px",
+                  }}
+                >
+                  <p style={{ fontWeight: "600", fontSize: "24px" }}>
+                    The Challenge We Solve
+                  </p>
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      gap: "16px",
+                    }}
+                  >
+                    <p className="story-p">
+                      With advancements in clinical research and the vibrant
+                      business environment, each biotech/pharmaceutical company
+                      has a unique pathway in clinical development. The main
+                      challenge companies face is balancing varying needs with
+                      investment in internal resources.
+                    </p>
+                    <p className="story-p">
+                      EffectStats was founded to provide the final solution - we
+                      work as your integrated data science team, tailoring our
+                      contribution based on your needs at different stages of
+                      development. With advancements in clinical research and
+                      the vibrant business environment, each
+                      biotech/pharmaceutical company has a unique pathway in
+                      clinical development. The main challenge companies face is
+                      balancing varying needs with investment in internal
+                      resources.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
