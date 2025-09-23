6bc3bcdfb6dd1687bce2c40150cd3f9cadfadd33
Liangzhijun123 <Liangzhijun1216@gmail.com>
2025-09-23 18:42:44 -0400

    added mcp client and server and fixed the prev errors. tried to do shiny python demo but got errors there ;

--- mcp/shell.nix
+++ mcp/shell.nix
@@ -1,13 +1,25 @@
 { pkgs ? import <nixpkgs> {} }:
+
 pkgs.mkShell {
   buildInputs = [
     pkgs.R
     pkgs.zlib
     pkgs.zlib.dev
-    pkgs.rPackages.remotes   # lowercase!
+    pkgs.rPackages.remotes
     pkgs.pkg-config
     pkgs.curl
     pkgs.openssl
     pkgs.libxml2
+    pkgs.gettext
+
+    # system deps for building packages
+    pkgs.nanomsg
+    pkgs.libuuid
+    pkgs.gcc
+    pkgs.gnumake
+
+    # R packages available in nixpkgs
+    pkgs.rPackages.nanonext
+    pkgs.rPackages.ellmer
   ];
 }
