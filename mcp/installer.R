bfa659c559090a0909cee0b83e6ab8b2d9786f15
Alex Lee <arl7816@g.rit.edu>
2025-09-27 13:06:47 -0400

    updates installer to use a const

--- mcp/installer.R
+++ mcp/installer.R
@@ -3,28 +3,30 @@
 LIB_CONST <- "~/R/library"
 
 # make sure the directory exists
-dir.create("~/R/library", recursive = TRUE, showWarnings = FALSE)
+dir.create(LIB_CONST, recursive = TRUE, showWarnings = FALSE)
 
 # add it to search paths
-.libPaths("~/R/library")
+.libPaths(LIB_CONST)
 
-install.packages("roxygen2", lib="~/R/library")
-install.packages("cli", lib="~/R/library")   # often needed too
-install.packages("pkgbuild", lib="~/R/library")
-install.packages("devtools", lib="~/R/library")
+install.packages("roxygen2", lib=LIB_CONST)
+install.packages("cli", lib=LIB_CONST)   # often needed too
+install.packages("pkgbuild", lib=LIB_CONST)
+install.packages("devtools", lib=LIB_CONST)
 install.packages("ambiorix", lib=LIB_CONST)
 
+install.packages("shiny", lib=LIB_CONST)
 
 # install remotes directly into ~/R/library
-install.packages("remotes", lib="~/R/library")
+install.packages("remotes", lib=LIB_CONST)
 install.packages("ellmer", lib=LIB_CONST)
 install.packages("dotenv", lib=LIB_CONST)
 
 # load remotes from there
-library(remotes, lib.loc="~/R/library")
+library(remotes, lib.loc=LIB_CONST)
 
 # now install mcpr (the engine behind this whole project)
-remotes::install_github("devOpifex/mcpr", lib="~/R/library")
+remotes::install_github("devOpifex/mcpr", lib=LIB_CONST)
+remotes::install_github("jrowen/rhandsontable", lib=LIB_CONST)
 
 # finally load mcpr
-library(mcpr, lib.loc="~/R/library")
+library(mcpr, lib.loc=LIB_CONST)
